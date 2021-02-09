81d89c50d74ebde780a33ef2979b9a9975cfd582
PeZet2 <47338686+PeZet2@users.noreply.github.com>
2021-02-09 17:49:00 +0100

    Update rzymskie.py

--- rzymskie.py
+++ rzymskie.py
@@ -1,4 +1,4 @@
-mapa = {
+mapa_ara_rzy = {
     1: "I",
     4: "IV",
     5: "V",
@@ -13,70 +13,85 @@ mapa = {
     900: "CM",
     1000: "M"
 }
+mapa_rzy_ara = {
+    "I": 1,
+    "IV": 4,
+    "V": 5,
+    "IX": 9,
+    "X": 10,
+    "XL": 40,
+    "L": 50,
+    "XC": 90,
+    "C": 100,
+    "CD": 400,
+    "D": 500,
+    "CM": 900,
+    "M": 1000
+}
+
+    
+    
+    
+
+def zamien_rz_do_ara(liczbaR):
+    liczbaA = 0
+    i = 0
+    while i < len(liczbaR):
+        
+        if i == (len(liczbaR)-1):
+            liczbaA += mapa_rzy_ara[liczbaR[i]]
+        else:
+            
+            try:
+                liczbaATemp = mapa_rzy_ara[liczbaR[i] + liczbaR[i+1]]
+                i += 1
+            except KeyError:
+                liczbaATemp = mapa_rzy_ara[liczbaR[i]]
+            
+            liczbaA += liczbaATemp
+            
+        i += 1    
+    
+    return liczbaA
 
 
-liczba = 322
+def najwiekszy_klucz(liczba):
+    global mapa_ara_rzy
+    
+    for i in sorted(mapa_ara_rzy, reverse=True):
+        if liczba >= i:
+            return i
+    
+    return 0
+    
 
-cyfry = []
-cyfry.append(int(liczba/1000))
-cyfry.append(int((liczba%1000)/100))
-cyfry.append(int(liczba%100/10))
-cyfry.append(int(liczba%10))
 
-print(liczba)
-print(cyfry[0])
-print(cyfry[1])
-print(cyfry[2])
-print(cyfry[3])
-
-rzymska = ""
-for i in range(0,len(cyfry)):
-    if cyfry[i] == 0:
-        continue
-
-    # indeks 0 czyli tysiace
-    if i == 0:
-        rzymska += mapa[1000]
-
-    # indeks 1 czyli setki
-    if i == 1:
-        if 1 <= cyfry[i] <= 3:
-            rzymska += cyfry[i] * mapa[100]
-        if cyfry[i] == 4:
-            rzymska += mapa[400]
-        if cyfry[i] == 5:
-            rzymska += mapa[500]
-        if 6 <= cyfry[i] <= 8:
-            rzymska += mapa[500] + (cyfry[i]-5)*mapa[100]
-        if cyfry[i] == 9:
-            rzymska += mapa[900]
-
-    # indeks 2 czyli dziesiatki
-    if i == 2:
-        if 1 <= cyfry[i] <= 3:
-            rzymska += cyfry[i] * mapa[10]
-        if cyfry[i] == 4:
-            rzymska += mapa[40]
-        if cyfry[i] == 5:
-            rzymska += mapa[50]
-        if 6 <= cyfry[i] <= 8:
-            rzymska += mapa[50] + (cyfry[i]-5)*mapa[10]
-        if cyfry[i] == 9:
-            rzymska += mapa[90]
-
-    # indeks 3 czyli jednosci
-    if i == 3:
-        if 1 <= cyfry[i] <= 3:
-            rzymska += cyfry[i] * mapa[1]
-        if cyfry[i] == 4:
-            rzymska += mapa[4]
-        if cyfry[i] == 5:
-            rzymska += mapa[5]
-        if 6 <= cyfry[i] <= 8:
-            rzymska += mapa[5] + (cyfry[i]-5)*mapa[1]
-        if cyfry[i] == 9:
-            rzymska += mapa[9]
-
-print("rzymska: " + rzymska)
+def zamien_ara_do_rz(liczbaA):
+    cyfry = [
+        int(liczbaA/1000),
+        int(liczbaA%1000-liczbaA%100),
+        int(liczbaA%100-liczbaA%10),
+        int(liczbaA%10)
+    ]
+    
+    liczbaR = ""
+    
+    while liczbaA > 0:
+        klucz = najwiekszy_klucz(liczbaA)
+        liczbaR += mapa_ara_rzy[klucz]
+        liczbaA -= klucz
+        
+    return liczbaR
+    
+    
+boo = True    
+while boo:
+	try:
+		liczbaR = [x for x in input().split(" ")]
+	except:
+		break
+	
+	Suma = zamien_rz_do_ara(liczbaR[0]) + zamien_rz_do_ara(liczbaR[1])
+	print(zamien_ara_do_rz(Suma))
 
 
